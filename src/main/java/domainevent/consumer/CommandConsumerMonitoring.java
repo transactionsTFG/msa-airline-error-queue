10396c7abd0a8403fa0882c45cced562d00b225c
josevillacres46 <jvillacr@ucm.es>
2025-04-13 18:47:56 +0200

    FEAT: Rollback to

--- src/main/java/domainevent/consumer/CommandConsumerMonitoring.java
+++ src/main/java/domainevent/consumer/CommandConsumerMonitoring.java
@@ -8,10 +8,12 @@ import javax.jms.TextMessage;
 
 import com.google.gson.Gson;
 
+import domainevent.publisher.IJMSCommandPublisher;
 import domainevent.services.MonitoringServices;
 import msa.commons.consts.JMSQueueNames;
 import msa.commons.consts.PropertiesConsumer;
 import msa.commons.event.Event;
+import msa.commons.event.EventId;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -21,6 +23,7 @@ import org.apache.logging.log4j.Logger;
 public class CommandConsumerMonitoring implements MessageListener {
     private Gson gson;
     private MonitoringServices monitoringServices;
+    private IJMSCommandPublisher jmsCommandPublisher;
     private static final Logger LOGGER = LogManager.getLogger(CommandConsumerMonitoring.class);
 
     @Override
@@ -31,6 +34,8 @@ public class CommandConsumerMonitoring implements MessageListener {
                 Event event = this.gson.fromJson(m.getText(), Event.class);
                 LOGGER.warn("Monitoreando en Cola {}, Evento Id: {}, Mensaje: {}", JMSQueueNames.AIRLINE_MONITORING_ERROR_QUEUE, event.getEventId(), event.getValue());
                 this.monitoringServices.saveError(origin, event.getEventId(), event.getValue());
+                for(EventId rollback : event.getValue().getRollbackEventTo()) 
+                    this.jmsCommandPublisher.publish(rollback, event.getValue());
             }
         } catch (Exception e) {
             LOGGER.error("Error al recibir el mensaje: {}", e.getMessage());
